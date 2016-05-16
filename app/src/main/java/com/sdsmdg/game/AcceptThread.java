4021c6f3bab4f379274f71059a9da60646625efe
Rahul Yadav <rahuly247@gmail.com>
2016-05-16 14:00:26 +0530

    Added Comments to the methods

---
+++ app/src/main/java/com/sdsmdg/game/AcceptThread.java
@@ -0,0 +1,7 @@
+package com.sdsmdg.game;
+
+/**
+ * Created by Rahul Yadav on 5/16/2016.
+ */
+public class AcceptThread {
+}
--- app/src/main/java/com/sdsmdg/game/MainActivity.java
+++ app/src/main/java/com/sdsmdg/game/MainActivity.java
@@ -15,14 +15,76 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    /***************************************************************************
+     * Initialization of variables
+     ****************************************************************************/
+
+    private static final String NAME = "Game";
     private static final String TAG = "com.sdsmdg.game";
+    private String UUID = "4cf6a202-fe8e-484e-89f7-0a546739b427";
+    private UUID MY_UUID = java.util.UUID.fromString(UUID);
     int REQUEST_ENABLE_BT = 1551;
+
+    /***************************************************************************
+     * Initialization of Objects
+     ***************************************************************************/
+    private BluetoothAdapter mBluetoothAdapter;
     private ArrayList<String> mDeviceList = new ArrayList<String>();
-    private ListView pairedListView;
-    private ListView unpairedListView;
+    private ListView pairedListView, unpairedListView;
+
+
+    /********************************************************************************
+     * Default method which launches itself as soon as activity starts
+     ********************************************************************************/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pairedListView = (ListView) findViewById(R.id.pairedListView);
+        unpairedListView = (ListView) findViewById(R.id.unpairedListView);
+
+        //Check if Bluetooth is available on the device
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(MainActivity.this, "No Bluetooth available !", Toast.LENGTH_SHORT).show();
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    /********************************************************************************
+          On find devices button Clicked
+     *******************************************************************************/
+
+    public void onFindDevicesClicked(View view) {
+
+        //First check if some paired devices are available or not
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                arrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+            pairedListView.setAdapter(arrayAdapter);
+        }
+        mBluetoothAdapter.startDiscovery();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+    }
+
+    /********************************************************************************
+     Broadcast receiver to discover bluetooth devices available in range
+     *******************************************************************************/
     private final BroadcastReceiver receiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -35,49 +97,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-    private BluetoothAdapter bluetoothAdapter;
-    private ArrayAdapter arrayAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        pairedListView = (ListView) findViewById(R.id.pairedListView);
-        unpairedListView = (ListView) findViewById(R.id.unpairedListView);
-        //Check if Bluetooth is available on the device
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            Toast.makeText(MainActivity.this, "No Bluetooth available !", Toast.LENGTH_SHORT).show();
-        } else if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-    }
-
-    public void onFindDevicesClicked(View view) {
-
-        //First check if some paired devices are available or not
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                arrayAdapter.add(device.getName() + "\n" + device.getAddress());
-            }
-            pairedListView.setAdapter(arrayAdapter);
-        }
-        bluetoothAdapter.startDiscovery();
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(receiver, filter);
-
-    }
-
+    /********************************************************************************
+     Stop listening for Intents as soon as activity gets destroyed
+     *******************************************************************************/
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(receiver);
     }
+    
+
+
 }
+
 
