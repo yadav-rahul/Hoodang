e9aa9c3e0eb762ce65fe1ab197142bc5ee4264e7
Rahul Yadav <rahuly247@gmail.com>
2016-05-20 23:47:15 +0530

    Added Custom View Class

--- app/src/main/java/com/sdsmdg/game/GameWorld/GameWorld.java
+++ app/src/main/java/com/sdsmdg/game/GameWorld/GameWorld.java
@@ -1,33 +1,76 @@
 package com.sdsmdg.game.GameWorld;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 
-import com.sdsmdg.game.Accelerometer.Accelerometer;
+import com.sdsmdg.game.MyView;
 
 /**
  * Created by Rahul Yadav on 5/20/2016.
  */
-public class GameWorld extends AppCompatActivity {
+public class GameWorld extends Activity implements SensorEventListener {
 
-    Accelerometer accelerometer;
-
+    static float aX;
+    static int height, width;
     SensorManager sensorManager;
     Sensor sensor;
+    MyView myView;
+
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        height = displaymetrics.heightPixels;
+        width = displaymetrics.widthPixels;
+
+        myView = new MyView(this);
+
+        setContentView(myView);
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        aX = event.values[0];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
-    public void initializeSensor(){
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager != null)
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager != null)
+            sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (sensorManager != null)
+            sensorManager.unregisterListener(this);
     }
 }
--- app/src/main/java/com/sdsmdg/game/MyView.java
+++ app/src/main/java/com/sdsmdg/game/MyView.java
@@ -1,16 +1,62 @@
 package com.sdsmdg.game;
 
 import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
  * Created by Rahul Yadav on 5/20/2016.
  */
-public class MyView extends SurfaceView {
+public class MyView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private final Paint paint;
+    private RectF rectF;
+    private RenderThread renderThread;
+
+    private float vX;
+    private float vY;
 
     public MyView(Context context) {
         super(context);
+
+        getHolder().addCallback(this);
+        renderThread = new RenderThread(getHolder(),this);
+
+        setFocusable(true);
+        paint = new Paint();
+        paint.setColor(0xFFFFFAAF);
+        paint.setAlpha(192);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setAntiAlias(true);
+
+        rectF= new RectF();
     }
 
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+
+    private class RenderThread extends Thread{
+        private SurfaceHolder surfaceHolder;
+        private MyView myView;
+        public RenderThread(SurfaceHolder surfaceHolder, MyView myView){
+            this.surfaceHolder = surfaceHolder;
+            this.myView = myView;
+        }
+    }
 }
