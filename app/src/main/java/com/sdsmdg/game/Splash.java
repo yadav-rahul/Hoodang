857f8f972d88b519a385cba15bbfa0c1178f135b
Rahul Yadav <rahuly247@gmail.com>
2016-06-06 22:02:28 +0530

    Updated UI

--- app/src/main/java/com/sdsmdg/game/Splash.java
+++ app/src/main/java/com/sdsmdg/game/Splash.java
@@ -42,7 +42,7 @@ public class Splash extends Activity {
                     try {
                         // Sleep for 200 milliseconds.
                         //Just to display the progress slowly
-                        Thread.sleep(10);
+                        Thread.sleep(30);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -58,6 +58,6 @@ public class Splash extends Activity {
                 startActivity(i);
                 finish();
             }
-        }, 1000);
+        }, 3000);
     }
 }
